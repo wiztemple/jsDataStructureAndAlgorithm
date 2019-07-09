4aefc64d196e4423fe7fa89dfa3c0402af575460
wiztemple <wiztemple@gmail.com>
2019-07-09 23:18:37 +0100

    insertion sort

--- sorting/index.js
+++ sorting/index.js
@@ -32,8 +32,8 @@ console.log(bubbleSort([4, 5, 1, 2, 6, 90, 34, 23, 13, 34, 23, 56, 78, 47, 0, 9]
 const selectionSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     let lowest = i;
-    for (let j = i + 1; j < arr.length) {
-      if (arr[j] > arr[lowest]);
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]);
       lowest = j;
     }
     let temp = arr[i];
@@ -43,5 +43,30 @@ const selectionSort = (arr) => {
 
   return arr;
 }
+
+const selSort = (arr) => {
+  const swap = (arr, idx1, idx2) => ([arr[idx1], arr[idx2] = [arr[idx2], arr[idx1]]]);
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]);
+      lowest = j;
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
+  return arr;
+}
  
-console.log(selectionSort([3,2,5,3,2,7,8,4,5]))
+console.log(selectionSort([3, 2, 5, 3, 2, 7, 8, 4, 5]));
+
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j+1] = arr[j]
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+console.log(insertionSort([3, 2, 5, 3, 2, 7, 8, 4, 5]));
